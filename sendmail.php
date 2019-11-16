cdeeef67ee38a860ee281835a044d77313cdf583
cquipu <ejoelhuillca@gmail.com>
2019-11-16 02:25:37 -0500

    web update

---
+++ sendmail.php
@@ -0,0 +1,6 @@
+<?php
+
+    $site_owners_email = 'edson.huillca@unmsm.edu.pe'; // Replace this with your own email address
+    $site_owners_name = 'Edson Web Site'; // replace with your name
+
+?>
